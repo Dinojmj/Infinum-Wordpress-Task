bc54e92fd490f4741135d7229187369f6876109b
dinojagust <dino.jagust@gmail.com>
2020-07-27 22:07:17 +0200

    [WP] - Added - ID fetch for images on blog post

--- wp-content/themes/uniduck/single.php
+++ wp-content/themes/uniduck/single.php
@@ -10,6 +10,18 @@ get_template_part('template-parts/blog-single');
 
 get_template_part('template-parts/related-blog');
 
+// Images for thumbnail
+$thumbnail_1x_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail_photo_1x');
+$thumbnail_2x_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail_photo_2x');
+
+// Images for featured post
+$featured_1x_url = get_the_post_thumbnail_url(get_the_ID(), 'featured_photo_1x');
+$featured_2x_url = get_the_post_thumbnail_url(get_the_ID(), 'featured_photo_2x');
+
+// Images for cover photo
+$cover_1x_url = get_the_post_thumbnail_url(get_the_ID(), 'cover_photo_1x');
+$cover_2x_url = get_the_post_thumbnail_url(get_the_ID(), 'cover_photo_2x');
+
 get_template_part('template-parts/footer');
 
 get_template_part('footer');
