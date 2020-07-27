f18e7030ebb31188dae54b38a10210fb2c2e314a
dinojagust <dino.jagust@gmail.com>
2020-07-27 22:10:58 +0200

    [WP] - Changed - Featured post made dynamic from static

--- wp-content/themes/uniduck/template-parts/featured-post.php
+++ wp-content/themes/uniduck/template-parts/featured-post.php
@@ -1,39 +1,61 @@
 <!-- Featured post START -->
 <section class="blog-post-featured">
+
+	<?php
+	if ( have_posts() ) : 
+	$count = 0;
+    while ( have_posts() ) : the_post();
+
+    	if ($count == 0) {
+    		?>
+
 	<div class="img-container">
-		<img class="image" src="resources/images/img-unicorn.png" srcset="resources/images/img-unicorn.png 1x, resources/images/img-unicorn@2x.png 2x" alt="Picture of unicorn">
+
+		<?php
+
+			$post_id = get_the_ID();
+			$featured_photo_1x_url = get_the_post_thumbnail_url($post_id, 'featured_photo_1x');
+			$featured_photo_2x_url = get_the_post_thumbnail_url($post_id, 'featured_photo_2x');
+
+			echo '<img class="image" src="' . $featured_photo_1x_url . '" srcset="' . $featured_photo_1x_url . ' 1x, ' . $featured_photo_2x_url . ' 2x" title="" alt="">';
+
+		?>
 	</div>
 
 	<div class="container-blog-featured">
-		<p class="blog-date">November 4, 2016</p>
+		<time class="blog-date"><?php the_time('F j, Y'); ?></time>
 		<h2 class="blog-post-title">
-			<a href="#">
-				The Legend of the Unicorn - Myths and Legends
+			<a href="<?php echo post_permalink() ?>">
+				<?php the_title(); ?>
 			</a>
 		</h2>
-		<ul class="blog-tags">
-			<li>
-				<a href="#">Unicorn</a>
-			</li>
-			<li>
-				<a href="#">Pinky</a>
-			</li>
-			<li>
-				<a href="#">Magic</a>
-			</li>
-			<li>
-				<a href="#">Love</a>
-			</li>
-		</ul>
-		<p class="post-text">Once upon a time in a kingdom far-far-away in the lands of the never-ending spring, a king sat in his golden throne and ruled his kingdom in perfect harmony. A person can feel nothing but exuberance at the sight of...<a class="read-more" href="#">Read more</a></p>
+
+		<?php
+			$tag_list = get_the_tag_list( '<ul class="blog-tags"><li>', '</li><li>', '</li></ul>' );
+
+			if ( $tag_list && ! is_wp_error( $tag_list ) ) {
+			    echo $tag_list;
+			} ?>
+		
+		<p class="post-text"><?php echo get_the_excerpt(); ?><a class="read-more" href="<?php echo post_permalink() ?>">Read more</a></p>
 		<ul class="post-info">
 			<li>
 				<a class="icon-favorits" href="#">17 faves</a>
 			</li>
 			<li>
-				<a class="icon-comments" href="#">22 comments</a>
+				<a class="icon-comments" href="#"><?php echo get_comments_number(); ?> comments</a>
 			</li>
 		</ul>
 	</div>
+	<?php
+	}
+	$count ++;
+
+	endwhile;
+	else :
+    	_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+	endif;
+	?>
+
 </section>
 <!-- Featured post END -->
