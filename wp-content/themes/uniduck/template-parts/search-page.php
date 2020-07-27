327ff218b40aff593b168608b6a7e0b1710a5849
dinojagust <dino.jagust@gmail.com>
2020-07-27 22:09:58 +0200

    [WP] - Changed - Search page PHP now working

--- wp-content/themes/uniduck/template-parts/search-page.php
+++ wp-content/themes/uniduck/template-parts/search-page.php
@@ -2,9 +2,9 @@
 <section class="search-page">
 	<h1 class="main-title">The Unicorn & a Duck</h1>
 
-	<form class="search-blog-form" role="search" method="get" action="#" id="">
-		<input class="search-icon" type="submit" value="" id="">
-		<input class="input-field" type="text" placeholder="Search blog" name="search" id="">
+	<form class="search-blog-form" role="search" method="get" action="/" id="search-blog">
+		<input class="search-icon" type="submit" value="" id="search-icon">
+		<input class="input-field" type="text" placeholder="Search blog" name="s" id="s">
 	</form>
 </section>
 <!-- Search page END -->
