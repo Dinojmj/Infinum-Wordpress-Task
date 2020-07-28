8fb70c4639fe635337273b7a09bc2389b03a78fa
dinojagust <dino.jagust@gmail.com>
2020-07-29 00:23:57 +0200

    [WP] - Added - CMS logo and navigation implemented in header

--- wp-content/themes/uniduck/template-parts/header.php
+++ wp-content/themes/uniduck/template-parts/header.php
@@ -1,21 +1,13 @@
 <!-- Header START -->
 <header class="global-header">
-	<a class="logo" href="#">
-		<img class="image" src="<?php bloginfo('template_url'); ?>/assets/images/Logo.png" srcset="<?php bloginfo('template_url'); ?>/assets/images/Logo.png 1x, <?php bloginfo('template_url'); ?>/assets/images/Logo@2x.png 2x" alt="Picture of unicorn">
-	</a>
-
+		
+	<?php
+	if ( function_exists( 'the_custom_logo' ) ) {
+		the_custom_logo();
+	}
+	?>
 	<nav>
-		<ul>
-			<li>
-				<a class="home" class="" href="#">Home</a>
-			</li>
-			<li>
-				<a class="button-pink" href="#">Get for IOS</a>
-			</li>
-			<li>
-				<a class="button-grey" href="#">Unicorn owners</a>
-			</li>
-		</ul>
+		<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu-header' ) ); ?>
 	</nav>
 </header>
 <!-- Header END -->
