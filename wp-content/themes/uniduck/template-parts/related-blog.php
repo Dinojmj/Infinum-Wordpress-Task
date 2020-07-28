e8339d51406f19426e35a6a738224f38b22c32d6
dinojagust <dino.jagust@gmail.com>
2020-07-29 00:21:03 +0200

    [WP] - Added - Related blog now shows previos post

--- wp-content/themes/uniduck/template-parts/related-blog.php
+++ wp-content/themes/uniduck/template-parts/related-blog.php
@@ -1,15 +1,41 @@
 <!-- Related Blog START -->
+
+<?php
+	$previous_post = get_previous_post();
+	if ($previous_post) {
+?>
+
 <section class="related-post">
 	<h2 class="related-title">More Magic</h2>
 
 	<div>
-		<img class="image" src="resources/images/img-unicorn5.png" srcset="resources/images/img-unicorn5.png 1x, resources/images/img-unicorn5@2x.png 2x" alt="Picture of unicorn duck">
+		<?php
+
+		$related_photo_1x_url = get_the_post_thumbnail_url($previous_post->ID, 'related_photo_1x');
+		$related_photo_2x_url = get_the_post_thumbnail_url($previous_post->ID, 'related_photo_2x');
+
+		echo '<img class="image" src="' . $related_photo_1x_url . '" srcset="' . $related_photo_1x_url . ' 1x, ' . $related_photo_2x_url . ' 2x" title="" alt="">';
+
+		?>
 
 		<div class="container-related">
-			<h3 class="blog-title">Sed ut perspiciatis unde omnis</h3>
-			<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit...</p>
-			<a class="read-more" href="#">Read more</a>
+			<h3 class="blog-title">
+				<?php echo $previous_post->post_title; ?>
+			</h3>
+			<p>
+				<?php
+
+				$post_excerpt = get_the_excerpt($previous_post->ID);
+				if ($post_excerpt) {
+					echo $post_excerpt;
+				}
+
+				?>
+
+			</p>
+			<a class="read-more" href="<?php echo get_permalink($previous_post->ID); ?>">Read more</a>
 		</div>
 	</div>
 </section>
 <!-- Related Blog END -->
+<?php } ?>
