a2f615be2b699f63aa5df36f8c147ea76f9979b4
dinojagust <dino.jagust@gmail.com>
2020-07-29 00:25:42 +0200

    [WP] - Removed - Post content from blog posts PHP moved to separet file

--- wp-content/themes/uniduck/template-parts/blog-posts.php
+++ wp-content/themes/uniduck/template-parts/blog-posts.php
@@ -2,69 +2,21 @@
 <section class="blog-posts">
 
 	<?php
+
+    global $wp_query;
+
 	if ( have_posts() ) : 
 	$count = 0;
     while ( have_posts() ) : the_post();
-
-    	if ($count >= 1) {
-    		?>
-
-    		<div class="blog-container">
-
-    			<div class="img-container">
-
-    				<?php
-
-    					$post_id = get_the_ID();
-    					$thumbnail_1x_url = get_the_post_thumbnail_url($post_id, 'thumbnail_photo_1x');
-    					$thumbnail_2x_url = get_the_post_thumbnail_url($post_id, 'thumbnail_photo_2x');
-
-    					echo '<img class="image" src="' . $thumbnail_1x_url . '" srcset="' . $thumbnail_1x_url . ' 1x, ' . $thumbnail_2x_url . ' 2x" title="" alt="">';
-
-    				?>
-
-    			</div>
-
-    			<div class="container-blogs">
-
-    				<time class="blog-date"><?php the_time('F j, Y'); ?></time>
-
-    				<h2 class="blog-post-title">
-    					<a href="<?php echo post_permalink() ?>">
-    						<?php the_title(); ?>
-    					</a>
-    				</h2>
-
-    				<?php
-    				$tag_list = get_the_tag_list( '<ul class="blog-tags"><li>', '</li><li>', '</li></ul>' );
-
-    				if ( $tag_list && ! is_wp_error( $tag_list ) ) {
-    				    echo $tag_list;
-    				} ?>
-
-    				<p class="post-text"><?php echo get_the_excerpt(); ?><a class="read-more" href="<?php echo post_permalink() ?>">Read more</a></p>
-    				<ul class="post-info">
-    					<li>
-    						<a class="icon-favorits" href="#">17 faves</a>
-    					</li>
-    					<li>
-    						<a class="icon-comments" href="#"><?php echo get_comments_number(); ?> comments</a>
-    					</li>
-    				</ul>
-
-    			</div>
-
-    		</div>
-
-    		<?php
-    	}
-    	$count ++;
+        	
+        get_template_part('template-parts/post-content');
 
 	endwhile;
-	else :
-    	_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+
+	else:
+        _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
 	endif;
 	?>
 
 </section>
-<!-- Blog Posts END
+<!-- Blog Posts END -->
