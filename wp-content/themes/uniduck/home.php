95625eef7f7847e42cfa11c0c206194e096d45fe
dinojagust <dino.jagust@gmail.com>
2020-07-29 00:28:07 +0200

    [WP] - Removed - Featured post removed from home PHP

--- wp-content/themes/uniduck/home.php
+++ wp-content/themes/uniduck/home.php
@@ -6,8 +6,6 @@ get_template_part('template-parts/header');
 
 get_template_part('template-parts/search-page');
 
-get_template_part('template-parts/featured-post');
-
 get_template_part('template-parts/blog-posts');
 
 get_template_part('template-parts/load-more');
