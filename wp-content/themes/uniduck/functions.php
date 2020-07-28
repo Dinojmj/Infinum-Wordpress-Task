efb34b09f3dda039da88232b493d1d4235935852
dinojagust <dino.jagust@gmail.com>
2020-07-29 00:51:02 +0200

    [WP] - Added - Supports for custom logo and menus and resolution generation for images

--- wp-content/themes/uniduck/functions.php
+++ wp-content/themes/uniduck/functions.php
@@ -5,6 +5,11 @@ include('custom-shortcodes.php');
 
 add_theme_support( 'post-thumbnails' );
 
+add_theme_support( 'custom-logo' );
+
+// Add menus in wp
+add_theme_support( 'menus' );
+
 remove_filter ('the_content', 'wpautop');
 remove_filter ('the_excerpt', 'wpautop');
 
@@ -19,6 +24,14 @@ add_image_size('featured_photo_2x', 1110, 704, true);
 //Image size for cover photos
 add_image_size('cover_photo_1x', 1441, 970, true);
 add_image_size('cover_photo_2x', 2882, 1940, true);
+
+//Image size for related photos
+add_image_size('related_photo_1x', 252, 210, true);
+add_image_size('related_photo_2x', 504, 420, true);
+
+//Image size for logo
+add_image_size('logo_image_1x', 172, 24, true);
+add_image_size('logo_image_2x', 344, 48, true);
 
 // Function to change the excerpt default 55 words to 32 words
 function wpdocs_custom_excerpt_length( $length ) {
