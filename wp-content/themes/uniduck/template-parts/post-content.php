2843ac401245d7b08263f5039d41715d96931f4d
Dino Jagust <dino.jagust@gmail.com>
2020-07-30 14:14:27 +0200

    [WP] - Added - Post content auto generate for image title and alt text

--- wp-content/themes/uniduck/template-parts/post-content.php
+++ wp-content/themes/uniduck/template-parts/post-content.php
@@ -1,21 +1,19 @@
 <div class="blog-container">
-
     <div class="img-container">
-
         <?php
-
             $post_id = get_the_ID();
             $thumbnail_1x_url = get_the_post_thumbnail_url($post_id, 'thumbnail_photo_1x');
             $thumbnail_2x_url = get_the_post_thumbnail_url($post_id, 'thumbnail_photo_2x');
 
-            echo '<img class="image" src="' . $thumbnail_1x_url . '" srcset="' . $thumbnail_1x_url . ' 1x, ' . $thumbnail_2x_url . ' 2x" title="" alt="">';
+            $image_id = get_post_thumbnail_id();
+    		$image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+    		$image_title = get_the_title($image_id);
 
+            echo '<img class="image" src="' . $thumbnail_1x_url . '" srcset="' . $thumbnail_1x_url . ' 1x, ' . $thumbnail_2x_url . ' 2x" title="' . $image_title . '" alt="' . $image_alt . '">';
         ?>
-
     </div>
 
     <div class="container-blogs">
-
         <time class="blog-date"><?php the_time('F j, Y'); ?></time>
 
         <h2 class="blog-post-title">
@@ -29,7 +27,8 @@
 
         if ( $tag_list && ! is_wp_error( $tag_list ) ) {
             echo $tag_list;
-        } ?>
+        }
+        ?>
 
         <p class="post-text"><?php echo get_the_excerpt(); ?><a class="read-more" href="<?php echo post_permalink() ?>">Read more</a></p>
         <ul class="post-info">
@@ -40,7 +39,5 @@
                 <span class="icon-comments"><?php echo get_comments_number(); ?> comments</span>
             </li>
         </ul>
-
     </div>
-
 </div>
