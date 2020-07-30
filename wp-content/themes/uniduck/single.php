db2dccbe4de6395ea9a99da793ee9e81c02eb25d
Dino Jagust <dino.jagust@gmail.com>
2020-07-30 14:16:44 +0200

    [WP] - Removed - Images for featured post

--- wp-content/themes/uniduck/single.php
+++ wp-content/themes/uniduck/single.php
@@ -14,10 +14,6 @@ get_template_part('template-parts/related-blog');
 $thumbnail_1x_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail_photo_1x');
 $thumbnail_2x_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail_photo_2x');
 
-// Images for featured post
-$featured_1x_url = get_the_post_thumbnail_url(get_the_ID(), 'featured_photo_1x');
-$featured_2x_url = get_the_post_thumbnail_url(get_the_ID(), 'featured_photo_2x');
-
 // Images for cover photo
 $cover_1x_url = get_the_post_thumbnail_url(get_the_ID(), 'cover_photo_1x');
 $cover_2x_url = get_the_post_thumbnail_url(get_the_ID(), 'cover_photo_2x');
