959c18b3f34ba017b66e9f6cd3182250f8535c72
Dino Jagust <dino.jagust@gmail.com>
2020-07-30 23:43:29 +0200

    [WP] - Added - Implemented customizer in footer for social media

--- wp-content/themes/uniduck/template-parts/footer.php
+++ wp-content/themes/uniduck/template-parts/footer.php
@@ -14,10 +14,29 @@
 		</div>
 
 		<ul class="social-media-footer">
-			<?php
-			if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Widget Area Footer Social") ) :
-			endif;
-			?>
+		<?php
+		$uniduck_social_options = get_option('uniduck_theme_options');
+		if($uniduck_social_options['fb_username'] && $uniduck_social_options['fb_link']) {
+		 ?>
+			<li>
+				Like <a href="<?php echo $uniduck_social_options['fb_link'];  ?>" target="_blank"><?php echo $uniduck_social_options['fb_username']; ?></a> on <a class="facebook" href="<?php echo $uniduck_social_options['fb_link']; ?>" target="_blank">facebook</a>
+			</li>
+		<?php
+		}
+
+		if($uniduck_social_options['tw_username'] && $uniduck_social_options['tw_link']) {
+		 ?>
+			<li>
+				Follow <a href="<?php echo $uniduck_social_options['tw_link'];  ?>" target="_blank">@<?php echo $uniduck_social_options['tw_username'];  ?></a> on <a class="twitter" href="<?php echo $uniduck_social_options['tw_link'];  ?>" target="_blank">twitter</a>
+			</li>
+		<?php
+		}
+		if($uniduck_social_options['ig_username'] && $uniduck_social_options['ig_link']) {
+		?>
+			<li>
+				Follow <a href="<?php echo $uniduck_social_options['ig_link'];  ?>" target="_blank">@<?php echo $uniduck_social_options['ig_username'];  ?></a> on <a class="instagram" href="<?php echo $uniduck_social_options['ig_link'];  ?>" target="_blank">instagram</a>
+			</li>
+		<?php } ?>
 		</ul>
 	</div>
 </footer>
