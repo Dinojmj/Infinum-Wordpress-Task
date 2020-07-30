662211bfb6dba0e9f1f12040c51f2f9199426f0e
Dino Jagust <dino.jagust@gmail.com>
2020-07-31 00:33:46 +0200

    [WP] - Fixed - Removed @ from html in footer social media

--- wp-content/themes/uniduck/template-parts/footer.php
+++ wp-content/themes/uniduck/template-parts/footer.php
@@ -27,14 +27,14 @@
 		if($uniduck_social_options['tw_username'] && $uniduck_social_options['tw_link']) {
 		 ?>
 			<li>
-				Follow <a href="<?php echo $uniduck_social_options['tw_link'];  ?>" target="_blank">@<?php echo $uniduck_social_options['tw_username'];  ?></a> on <a class="twitter" href="<?php echo $uniduck_social_options['tw_link'];  ?>" target="_blank">twitter</a>
+				Follow <a href="<?php echo $uniduck_social_options['tw_link'];  ?>" target="_blank"><?php echo $uniduck_social_options['tw_username'];  ?></a> on <a class="twitter" href="<?php echo $uniduck_social_options['tw_link'];  ?>" target="_blank">twitter</a>
 			</li>
 		<?php
 		}
 		if($uniduck_social_options['ig_username'] && $uniduck_social_options['ig_link']) {
 		?>
 			<li>
-				Follow <a href="<?php echo $uniduck_social_options['ig_link'];  ?>" target="_blank">@<?php echo $uniduck_social_options['ig_username'];  ?></a> on <a class="instagram" href="<?php echo $uniduck_social_options['ig_link'];  ?>" target="_blank">instagram</a>
+				Follow <a href="<?php echo $uniduck_social_options['ig_link'];  ?>" target="_blank"><?php echo $uniduck_social_options['ig_username'];  ?></a> on <a class="instagram" href="<?php echo $uniduck_social_options['ig_link'];  ?>" target="_blank">instagram</a>
 			</li>
 		<?php } ?>
 		</ul>
