9175a086244b8c13d4916d73e6f0593bc303ce70
Dino Jagust <dino.jagust@gmail.com>
2020-07-30 14:18:43 +0200

    [WP] - Added - Index fallback

--- wp-content/themes/uniduck/index.php
+++ wp-content/themes/uniduck/index.php
@@ -1,5 +1,11 @@
-<?php 
+<?php
 
 get_template_part('header');
+
+get_template_part('template-parts/header');
+
+get_template_part('template-parts/blog-posts');
+
+get_template_part('template-parts/footer');
 
 get_template_part('footer');
